7428f9ae58f1acaaec44b8f5c3758373887f16ff
Sergey <lazybark.dev@gmail.com>
2023-01-27 15:10:18 +0300

    k-version

---
+++ cmd/v1k/main.go
@@ -0,0 +1,26 @@
+// v1k is a special version for my own use. It has some pre-defined settings and nothing more.
+package main
+
+import (
+	"log"
+
+	"github.com/lazybark/go-img-downloader/config"
+	"github.com/lazybark/go-img-downloader/pkg/menu"
+)
+
+func main() {
+	cfg, err := config.InitApp()
+	if err != nil {
+		log.Fatal("[APP INIT ERROR]", err)
+		return
+	}
+
+	cfg.ConvertAllToJPG = true
+	cfg.ForceChrome = true
+	cfg.MinImgHeight = 251
+	cfg.MaxImgWidth = 251
+	cfg.MaxImgHeight = 1200
+	cfg.MaxImgWidth = 1200
+
+	menu.PromtMainMenu(cfg)
+}
